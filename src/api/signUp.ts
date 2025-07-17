3913e90b4b995fff7525a75a79577ed07e97ce20
Bezawit Teshome <bezateshome1234@gmail.com>
2025-07-17 18:53:22 +0300

    fix: replace the type defined in signup

--- src/api/signUp.ts
+++ src/api/signUp.ts
@@ -2,7 +2,6 @@
 import { SignUpData } from "@/types/SignUp";
 import axios from "axios";
 
-
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const signUp = async (data: SignUpData) => {
   const response = await axios.post(`${BASE_URL}/signup/`, data);
--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -1,5 +1,5 @@
 // src/hooks/useSignUpMutation.ts
-import { signUp} from "@/api/signUp";
+import { signUp } from "@/api/signUp";
 import { SignUpData } from "@/types/SignUp";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
